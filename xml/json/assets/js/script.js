a28595242e03e809bea04d5020fb4fed373b5ac3
farihafari <fareehajabeen62@gmail.com>
2024-10-08 10:55:31 +0500

    detail

--- xml/json/assets/js/script.js
+++ xml/json/assets/js/script.js
@@ -4,23 +4,42 @@ $(document).ready(function(){
         type:"get",
         success:function(brands){
 // console.log(brands)
-let x ="";
+let apple ="";
+let realme = "";
 $.each(brands,function(keys,arrays){
-  $.each(arrays,function(index,objects){
-x+=`    <div class="col-lg-3 mt-3">
-                <div class="card">
-                    <img class="card-img-top" src="${objects.image}" alt="Title" />
-                    <div class="card-body">
-                        <h4 class="card-title">${objects.name}</h4>
-                        <p class="card-text">Rs: ${objects.price}</p>
-                        <a href = "" class="btn btn-info">detail</a>
-                    </div>
-                </div>
-                
-            </div>`
-  })
+    if(keys=="apple"){
+
+        $.each(arrays,function(index,objects){
+      apple+=`    <div class="col-lg-3 mt-3">
+                      <div class="card">
+                          <img class="card-img-top" src="${objects.image}" alt="Title" />
+                          <div class="card-body">
+                              <h4 class="card-title">${objects.name}</h4>
+                              <p class="card-text">Rs: ${objects.price}</p>
+                              <a href = "detail.html?product=${keys+index}" class="btn btn-info">detail</a>
+                          </div>
+                      </div>
+                      
+                  </div>`
+        })
+    }else if(keys=="realme"){
+        $.each(arrays,function(index,objects){
+            realme+=`    <div class="col-lg-3 mt-3">
+                            <div class="card">
+                                <img class="card-img-top" src="${objects.image}" alt="Title" />
+                                <div class="card-body">
+                                    <h4 class="card-title">${objects.name}</h4>
+                                    <p class="card-text">Rs: ${objects.price}</p>
+                                    <a href = "detail.html?product=${keys+index}=" class="btn btn-info">detail</a>
+                                </div>
+                            </div>
+                            
+                        </div>`
+              })
+    }
 })
-$("#data").html(x)
+$("#datarealme").html(realme)
+$("#data").html(apple)
         }
     })
 })
